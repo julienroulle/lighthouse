021d1748385df3c71c1092594e79ad71759fefeb
Julien Roullé <ju.roulle@gmail.com>
2022-01-27 08:30:30 -0500

    feat: new simple board for lighthouse

---
+++ src/Board.tsx
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2017 The boardgame.io Authors.
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+import React from 'react';
+import { BoardProps } from 'boardgame.io/react';
+import { TicTacToeState } from './Game';
+
+import './board.css';
+
+interface TicTacToeProps extends BoardProps<TicTacToeState> {}
+
+
+const blueCells  = [30, 32, 33, 34, 44, 45, 47, 59, 60, 62, 73, 75, 76, 87, 88]
+const purpleCells = [37, 38, 49, 50, 51, 53, 63, 64, 65, 77, 78, 79, 81, 92, 93, 94, 95]
+const orangeCells = [89, 90, 100, 102, 103, 104, 114, 115, 116, 117, 118, 129, 130, 131, 142, 144, 146, 156, 157, 159, 160]
+const greenCells  = [106, 107, 108, 109, 119, 120, 121, 122, 133, 134, 136, 137, 147, 148, 149, 151, 163, 164, 165]
+const blackCells  = [61, 66, 143, 150]
+const greyCells  = [31, 46, 48, 52, 58, 67, 64, 74, 80, 86, 101, 123, 128, 135, 158, 162]
+
+export const Board = ({ G, ctx, moves }: TicTacToeProps) => {
+
+    const checkIsActive = (row, clm) => {
+        if (G.cells[row][clm] !== null) return false;
+        return true;
+      }
+
+  const onClick = (row, clm) => {
+    if (checkIsActive(row, clm)) {
+      moves.clickCell(row, clm);
+    }
+  };
+
+    let tbody: Array<JSX.Element> = [];
+    for (let row = 0; row < 14; row++) {
+        let cells: Array<JSX.Element> = [];
+        for (let clm = 0; clm < 14; clm++) {
+            const id = 14 * row + clm;
+            let color = ''
+            if (blueCells.includes(id)) {
+                color = 'active blue'
+            } else if (purpleCells.includes(id)) {
+                color = 'active purple'
+            } else if (orangeCells.includes(id)) {
+                color = 'active orange'
+            } else if (greenCells.includes(id)) {
+                color = 'active green'
+            } else if (blackCells.includes(id)) {
+                color = 'black'
+            } else if (greyCells.includes(id)) {
+                color = 'active grey'
+            }
+        cells.push(
+            <td
+                key={id}
+                className={checkIsActive(row, clm) ? `${color}` : ''}
+                onClick={() => onClick(row, clm)}
+                >
+                {G.cells[row][clm]}
+            </td>
+        );
+        }
+        tbody.push(<tr key={row}>{cells}</tr>);
+    }
+
+    let winner: JSX.Element = <div/>;
+    if (ctx.gameover) {
+        winner =
+        ctx.gameover.winner !== undefined ? (
+            <div id="winner">Winner: {ctx.gameover.winner}</div>
+        ) : (
+            <div id="winner">Draw!</div>
+            );
+    }
+
+    return (
+        <div>
+        <table id="board">
+            <tbody>{tbody}</tbody>
+        </table>
+        {winner}
+        </div>
+    );
+}
+
+export default Board;
